7f95a9b09881e443f5e21b029a1980a55a9d65bd
Sean Murphy <sean@iamseanmurphy.com>
2026-07-17 14:00:26 +0000

    2.2.2

--- responsive-addons-for-elementor/admin/js/dashboard/index.asset.php
+++ responsive-addons-for-elementor/admin/js/dashboard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ee868813181b2bdc2322');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e021b21cec3d1db21be9');
